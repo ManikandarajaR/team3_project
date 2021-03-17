76510bad9f0e8f471def8135b331d97f71aafb6e
ManikandarajaR <76580192+ManikandarajaR@users.noreply.github.com>
2021-03-17 21:55:37 +0530

    Add files via upload

--- src/offer.js
+++ src/offer.js
@@ -3,43 +3,72 @@ import "./css/offer.css"
 
 const offer = () =>{
      return(
-          <section id="offer">
-
-          <h1 className="text-center pt-5">Popular Destinations Offered</h1>
-          <p className="text-center mt-3">World's best tourist destination</p>
-          
-          <div class="container">
-             
-                    <div className="card-width">
-                         <img src="https://demo.wenthemes.com/travel-gem/wp-content/uploads/sites/65/2018/10/tirp11-400x500.jpg"/>
-                         <div className="card-content">
-                              <p>Australia</p>
-                         </div>  
-                    </div>
-                    
-                    <div className="card-width">
-                    <img href="" src="https://demo.wenthemes.com/travel-gem/wp-content/uploads/sites/65/2018/10/trip-12-400x500.jpg"/>
-                         <div className="card-content">
-                              <p><a href="">France</a></p>
-                         </div>  
-                         
-                    </div>
-                    
-                    <div className="card-width">
-                         <img href="" src="https://demo.wenthemes.com/travel-gem/wp-content/uploads/sites/65/2018/03/trip6-400x500.jpg"/>
-                         <div className="card-content">
-                              <p>Italy</p>
-                         </div>  
-                    </div>
-                    <div className="card-width">
-                         <img src="https://demo.wenthemes.com/travel-gem/wp-content/uploads/sites/65/2018/10/tirp11-400x500.jpg"/>
-                         <div className="card-content">
-                              <p>Sydney</p>
+          <div>
+          <footer>
+          <div className="container">
+               <div className="footer-top">
+               <div className="row">
+                    <div className="col-md-3">
+                         <div className="title">
+                              <h4>About Us !!</h4>
+                              <hr/>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis metus scelerisque, faucibus risus eu, luctus est. Vestibulum ante ipsum primis in faucibusLorem ipsum dolor sit amet,</p>
                          </div>
-                     </div>
-                </div>
-       
-          </section>
+                    </div>
+
+                    <div className="col-md-3">
+                         <div className="title">
+                              <h4>Recent Blogs</h4>
+                              <hr/>
+                              
+                                   <a href="">Vacation</a>
+                                <br/>
+                                    <a href="">Recent posts</a>
+                                 <br/>  
+                                   <a href="">Images</a>
+                                    
+                         </div>
+                    </div>
+                    <div className="col-md-3  ">
+                         <div className="title">
+                         <h4>Gallery </h4>
+                         <hr/>
+                         <div className="row footer-b footer-img">
+                                   <div className="col-md-4">
+                                        <img src="https://i.pinimg.com/originals/28/bc/d0/28bcd0d7e9310764a99dbb27c65cdf60.jpg"/>
+                                   </div>
+                                   <div className="col-md-4">
+                                        <img src="https://images-cdn.9gag.com/photo/aMY7d5G_700b.jpg"/>
+                                   </div>
+                                   <div className="col-md-4">
+                                        <img src="https://i.pinimg.com/originals/a1/1f/b4/a11fb432a03b2eea292ae39591d149ec.jpg"/>
+                                   </div>
+                              </div>
+                         <div className="row footer-img">
+                              <div className="col-md-4">
+                                   <img src="https://i.pinimg.com/originals/28/bc/d0/28bcd0d7e9310764a99dbb27c65cdf60.jpg"/>
+                              </div>
+                              <div className="col-md-4">
+                                   <img src="https://images-cdn.9gag.com/photo/aMY7d5G_700b.jpg"/>
+                              </div>
+                              <div className="col-md-4">
+                                   <img src="https://i.pinimg.com/originals/a1/1f/b4/a11fb432a03b2eea292ae39591d149ec.jpg"/>
+                              </div>
+                         </div>
+                    </div>
+                    </div>
+                    <div className="col-md-3 dubai">
+                    <div className="title">
+                    <h4>Address</h4>
+                    <hr/>
+                    <p>Number 6 , <br/>Vivekananthar theru, <br/> Dubai Main road, <br/> Dubai Kuruku sandhu, <br/> Dubai </p>
+               </div>
+                    </div>
+               </div>
+          </div>
+          </div>
+     </footer>
+          </div>
         
      )
 }
